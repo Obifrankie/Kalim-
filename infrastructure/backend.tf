94f7eec53740d6913105a839ae5478ebf4e6bdb0
obifrankie <obifranklina@gmail.com>
2024-07-04 03:54:42 +0100

    Final touches

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kalim-terraform-backend" # Replace with your S3 bucket name
+    bucket = "s3-bucjet" # Replace with your S3 bucket name
     key    = "terraform.tfstate"       # Replace with your desired state file name
     region = "eu-west-1"               # Replace with your desired AWS region
     # profile        = "default"                      # Replace with your AWS profile if not using default
